dc54ef71216f6ccd18615828f20ee694688d8649
chehachraf <achrafchehboun@outlook.com>
2025-10-20 10:59:43 +0100

    first commit

---
+++ src/main/java/com/multitrans/wasalliya/WasalliyaApplication.java
@@ -0,0 +1,13 @@
+package com.multitrans.wasalliya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasalliyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasalliyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multitrans/wasalliya/WasalliyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitrans.wasalliya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasalliyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
