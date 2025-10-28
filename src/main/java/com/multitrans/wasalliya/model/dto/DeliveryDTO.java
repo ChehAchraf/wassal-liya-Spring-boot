0e13d5fcf71ea1904b70e00bc5987b5b927e9d73
chehachraf <achrafchehboun@outlook.com>
2025-10-28 20:00:13 +0100

    add some enhacements to some controllers

--- src/main/java/com/multitrans/wasalliya/model/dto/DeliveryDTO.java
+++ src/main/java/com/multitrans/wasalliya/model/dto/DeliveryDTO.java
@@ -2,10 +2,12 @@ package com.multitrans.wasalliya.model.dto;
 
 import com.multitrans.wasalliya.enums.DeliveryStatus;
 import com.multitrans.wasalliya.model.Tour;
+import jakarta.validation.constraints.NotNull;
 
 
 public record DeliveryDTO(
         Long id,
+        @NotNull(message = "Please the address field is required")
         String address,
         Double latitude,
         Double longitude,
