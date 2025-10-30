6fd11596d710639ef55edf090ca0befb1002155e
chehachraf <achrafchehboun@outlook.com>
2025-10-30 11:26:24 +0100

    complete adding logger for warehouse

--- src/main/java/com/multitrans/wasalliya/service/WarehouseService.java
+++ src/main/java/com/multitrans/wasalliya/service/WarehouseService.java
@@ -7,9 +7,11 @@ import com.multitrans.wasalliya.model.mapper.WarehouseMapper;
 import com.multitrans.wasalliya.model.Warehouse;
 import com.multitrans.wasalliya.repository.WarehouseRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 public class WarehouseService {
     private final WarehouseRepository warehouseRepo;
     private final WarehouseMapper wMapper;
@@ -34,20 +36,25 @@ public class WarehouseService {
     public WarehouseDTO findWarehouseById(Long id) {
         logger.logInfo("Attempt to find a ware house");
         Warehouse foundedWarehouse = warehouseRepo.findById(id).orElseThrow(NoSuchElementException::new);
+        logger.logInfo("Warehouse founded with id : " + foundedWarehouse.getId());
         return wMapper.toDTO(foundedWarehouse);
     }
 
     @Transactional
     public String deleteWarehouseById(Long id) {
+        logger.logInfo("Attempt to delete warehouse by id...");
         Warehouse warehouseToDelete = warehouseRepo.findById(id).orElseThrow(NoSuchElementException::new);
         warehouseRepo.deleteById(id);
+        logger.logInfo("Warehouse with id " + warehouseToDelete.getId() +" Deleted" );
         return "Ware house with id " + id + " Has been deleted Seccussfully";
     }
 
     @Transactional
     public WarehouseDTO updateWarehouseById(Long id, WarehouseDTO dto) {
+        logger.logInfo("Attempt to update warehouse");
         Warehouse warehouseToEdit = warehouseRepo.findById(id).orElseThrow(NoSuchElementException::new);
         wMapper.updateFromDTO(dto, warehouseToEdit);
+        logger.logInfo("Warehouse has been updated , id : " + warehouseToEdit.getId());
         return wMapper.toDTO(warehouseRepo.save(warehouseToEdit));
     }
 
